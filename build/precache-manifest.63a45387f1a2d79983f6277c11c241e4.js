1025dc96ff9d7d48466f9653835647e93955eb5c
hyrdbyrd <IRuss23@yandex.ru>
2019-02-08 18:22:43 +0300

    chore: initial commit

---
+++ build/precache-manifest.63a45387f1a2d79983f6277c11c241e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ed1c4602d1ac2345e23a",
+    "url": "./static/js/main.ed1c4602.chunk.js"
+  },
+  {
+    "revision": "0b36ecb97d70038e42c1",
+    "url": "./static/js/1.0b36ecb9.chunk.js"
+  },
+  {
+    "revision": "ed1c4602d1ac2345e23a",
+    "url": "./static/css/main.08c52592.chunk.css"
+  },
+  {
+    "revision": "2d22eaf4ec1ae9a8d62c87e68a9f209e",
+    "url": "./index.html"
+  }
+];
